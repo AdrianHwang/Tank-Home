21f956234c546ee6c05ec9034db8918d1594dc56
AdrianHwang <boardsky@126.com>
2018-06-23 12:36:36 +0800

    过滤静态路径

--- main.go
+++ main.go
@@ -3,9 +3,29 @@ package main
 import (
 	_ "Tank-Home/routers"
 	"github.com/astaxie/beego"
+	"net/http"
+	"strings"
+	"github.com/astaxie/beego/context"
 )
 
 func main() {
+	ignoreStaticPath()
 	beego.Run()
 }
 
+func ignoreStaticPath(){
+	//过滤静态目录
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
+}
+
+func TransparentStatic(ctx *context.Context){
+	orPath:=ctx.Request.URL.Path
+	beego.Debug("request url:", orPath)
+
+	if strings.Index(orPath, "api")>=0{
+		return
+	}
+
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+orPath)
+}
